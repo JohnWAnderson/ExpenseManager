bb08753a88487e0e12d01df848c47e9b2e080792
JohnWAnderson <John.Anderson45@yahoo.com>
2018-10-09 10:00:20 -0700

    Date Selector working on date selection page

--- app-front-end/src/Redux/selector.js
+++ app-front-end/src/Redux/selector.js
@@ -2,16 +2,10 @@ import moment from 'moment';
 
 export default (items, {name, sortby, startDate, endDate}) =>{
     return items.filter((item) => {
-        const createdAtMoment = moment(items.duedate)
-        // console.log('amount',createdAtMoment);
-        // console.log('start', moment(startDate));
-        // console.log(item.name);
-        // console.log(name);
-        
+        const createdAtMoment = moment(item.duedate)
         const startDateMatch = startDate ? moment(startDate).isSameOrBefore(createdAtMoment, 'day') : true;
         const endDateMatch = endDate ? moment(endDate).isSameOrAfter(createdAtMoment, 'day') : true;
         const nameMatch = item.name.toLowerCase().includes(name.toLowerCase());   
-        //console.log(item.name, startDateMatch, endDateMatch, nameMatch)
         return startDateMatch && endDateMatch && nameMatch
     }).sort((a,b) => {
         if(sortby === 'date'){
